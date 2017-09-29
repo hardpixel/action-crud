ed24a043e5ea9f40fdd753414f115a15d0455cc3
Jonian Guveli <jonian@hardpixel.eu>
2017-09-29 16:41:36 +0300

    update main module

--- lib/action_crud.rb
+++ lib/action_crud.rb
@@ -1,5 +1,25 @@
+require 'action_crud/controller'
+require 'action_crud/pagination'
+require 'action_crud/helpers'
 require 'action_crud/version'
 
 module ActionCrud
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  included do
+    # Include submodules
+    include ActionCrud::Controller
+    include ActionCrud::Pagination
+  end
+end
+
+# Include action view helpers
+if defined? ActionView::Base
+  ActionView::Base.send :include, ActionCrud::Helpers
+  ActionView::Base.send :include, ActionCrud::Helpers::Records
+end
+
+# Include action controller helpers
+if defined? ActionController::Base
+  ActionController::Base.send :include, ActionCrud::Helpers
 end
