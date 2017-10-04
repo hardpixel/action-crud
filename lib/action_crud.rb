e480d6dff77159b603caea17f186bb0b4fcde46d
Jonian Guveli <jonian@hardpixel.eu>
2017-10-04 17:24:34 +0300

    rename general helpers to url

--- lib/action_crud.rb
+++ lib/action_crud.rb
@@ -1,6 +1,9 @@
 require 'action_crud/controller'
 require 'action_crud/pagination'
-require 'action_crud/helpers'
+require 'action_crud/helpers/route'
+require 'action_crud/helpers/link'
+require 'action_crud/helpers/url'
+require 'action_crud/helpers/view'
 require 'action_crud/version'
 
 module ActionCrud
@@ -15,11 +18,11 @@ end
 
 # Include action view helpers
 if defined? ActionView::Base
-  ActionView::Base.send :include, ActionCrud::Helpers
+  ActionView::Base.send :include, ActionCrud::Helpers::Url
   ActionView::Base.send :include, ActionCrud::Helpers::View
 end
 
 # Include action controller helpers
 if defined? ActionController::Base
-  ActionController::Base.send :include, ActionCrud::Helpers
+  ActionController::Base.send :include, ActionCrud::Helpers::Url
 end
